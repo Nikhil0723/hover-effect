ece9631431104bbdf673d739b5f88acbdd58cc1f
Nikhil Vishwakarma <138506850+Nikhil0723@users.noreply.github.com>
2023-08-04 02:21:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+const wrapper = document.getElementById("cards");
+
+let columns = 0,
+    rows = 0,
+    toggled = false;
+
+
+
+const createTile = index => {
+  const card = document.createElement("div");
+  
+  card.classList.add("card");
+  
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("card-content");
+  
+  card.appendChild(cardContent)
+  
+  return card;
+}
+
+const createTiles = quantity => {
+  Array.from(Array(quantity)).map((tile, index) => {
+    wrapper.appendChild(createTile(index));
+  });
+}
+
+const createGrid = () => {
+  wrapper.innerHTML = "";
+  
+  const size = document.body.clientWidth > 800 ? 100 : 50;
+  
+  columns = Math.floor(document.body.clientWidth / size);
+  rows = Math.floor(document.body.clientHeight / size);
+  
+  wrapper.style.setProperty("--columns", columns);
+  wrapper.style.setProperty("--rows", rows);
+  
+  createTiles(columns * rows);
+}
+
+createGrid();
+
+window.onresize = () => createGrid();
+
+
 document.getElementById("cards").onmousemove = e => {
     for(const card of document.getElementsByClassName("card")) {
       const rect = card.getBoundingClientRect(),
