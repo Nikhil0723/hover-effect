7ead76ee147b24e400f90b8ddebaca7b5e2cbbc9
Nikhil Vishwakarma <138506850+Nikhil0723@users.noreply.github.com>
2023-08-04 02:32:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,56 +1,52 @@
 const wrapper = document.getElementById("cards");
 
-let columns = 0,
-    rows = 0,
-    toggled = false;
-
-
-
-const createTile = index => {
+const createTile = (index) => {
   const card = document.createElement("div");
-  
   card.classList.add("card");
-  
+
   const cardContent = document.createElement("div");
   cardContent.classList.add("card-content");
-  
-  card.appendChild(cardContent)
-  
-  return card;
-}
+  cardContent.innerText = `Card ${index + 1}`;
 
-const createTiles = quantity => {
-  Array.from(Array(quantity)).map((tile, index) => {
+  card.appendChild(cardContent);
+
+  return card;
+};
+
+const createTiles = (quantity) => {
+  Array.from(Array(quantity)).forEach((_, index) => {
     wrapper.appendChild(createTile(index));
   });
-}
+};
 
 const createGrid = () => {
   wrapper.innerHTML = "";
-  
+
   const size = document.body.clientWidth > 800 ? 100 : 50;
-  
-  columns = Math.floor(document.body.clientWidth / size);
-  rows = Math.floor(document.body.clientHeight / size);
-  
+
+  const columns = Math.floor(document.body.clientWidth / size);
+  const rows = Math.floor(document.body.clientHeight / size);
+
   wrapper.style.setProperty("--columns", columns);
   wrapper.style.setProperty("--rows", rows);
-  
+
   createTiles(columns * rows);
-}
+};
 
 createGrid();
 
 window.onresize = () => createGrid();
 
+document.getElementById("cards").onmousemove = (e) => {
+  const cards = document.getElementsByClassName("card");
+  const cardWrapperRect = wrapper.getBoundingClientRect();
 
-document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
+  for (const card of cards) {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left - cardWrapperRect.left;
+    const y = e.clientY - rect.top - cardWrapperRect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
   }
+};
